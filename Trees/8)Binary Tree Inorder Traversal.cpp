d03533b478966d60a69070fedfa259e33d24ac89
jigyasaG18 <158419387+jigyasaG18@users.noreply.github.com>
2024-12-31 11:56:16 +0530

    Add files via upload

---
+++ Trees/8)Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,53 @@
+Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>inorder;
+        stack<TreeNode*>st;
+        TreeNode* node=root;
+        while(true){
+            if(node!=nullptr){
+                st.push(node);
+                node=node->left;
+            }
+            else//if node is null
+            {
+               if(st.empty()==true) break;
+               node=st.top();
+               st.pop();
+               inorder.push_back(node->val);
+               node=node->right;
+            }
+        }
+    return inorder;
+    }
+};
