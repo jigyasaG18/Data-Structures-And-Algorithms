02a3d6a9130461e816db89b6640c8a909c5439d5
jigyasaG18 <158419387+jigyasaG18@users.noreply.github.com>
2025-01-10 21:24:57 +0530

    Add files via upload

---
+++ Linked List/2)Swap Nodes in Pairs.cpp
@@ -0,0 +1,48 @@
+Swap Nodes in Pairs
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+
+Example 4:
+Input: head = [1,2,3]
+Output: [2,1,3]
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode* dnode=new ListNode(-1);
+        ListNode* prev=dnode;
+        ListNode* curr=head;
+
+        while(curr!=nullptr && curr->next!=nullptr){
+            prev->next=curr->next;
+            curr->next=prev->next->next;
+            prev->next->next=curr;
+
+            prev=curr;
+            curr=curr->next;
+        }
+        return dnode->next;
+    }
+};
