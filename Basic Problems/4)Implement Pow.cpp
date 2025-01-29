ae353a181ad2b28b532e2d3f8afba59e551d0f7b
jigyasaG18 <158419387+jigyasaG18@users.noreply.github.com>
2025-01-29 11:44:22 +0530

    Add files via upload

---
+++ Basic Problems/4)Implement Pow.cpp
@@ -0,0 +1,21 @@
+Implement Pow
+Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+
+Examples:
+Input: b = 3.00000, e = 5
+Output: 243.00000
+
+Input: b = 0.55000, e = 3
+Output: 0.16638
+
+Input: b = -0.67000, e = -7
+Output: -16.49971
+
+// User function Template for C++
+class Solution {
+  public:
+    double power(double b, int e) {
+        // code here
+        return pow(b,e);
+    }
+};
